a87b5f9ea1eb9a9af1e038eae709a267202ea946
Thomas Löhr <thomas@loehr.co.uk>
2022-06-16 19:38:20 +0100

    built docs

--- kitchensink/io/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Routines for reading and writing various outputs"""
-
-from .rw import (hex_to_bin, handle_path, patch, working_dir,
-                 is_plumed, is_same_shape, read_plumed_fields,
-                 read_plumed_df, read_multi, read_plumed,
-                 plumed_iterator, file_length, field_glob)
-
-from .hdf import h5tree
